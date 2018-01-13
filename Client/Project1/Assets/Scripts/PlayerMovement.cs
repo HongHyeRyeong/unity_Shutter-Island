e65083f3292c6cadc7b604a41b3509b30f3214ee
HongHyeRyeong <h0412l@naver.com>
2018-01-13 19:16:27 +0900

    플레이어 이동 수정

--- Client/Project1/Assets/Scripts/PlayerMovement.cs
+++ Client/Project1/Assets/Scripts/PlayerMovement.cs
@@ -3,27 +3,39 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private float h = 0.0f;
-    private float v = 0.0f;
-
-    private Transform tr;
-    
-    float Movespeed = 5.0f;
-    float Rotspeed = 150.0f;
-
-    void Start()
-    {
-        tr = GetComponent<Transform>();            
-    }
+    public float moveSpeed = 5f;
+    public float rotationSpeed = 50f;
 
     void Update()
     {
-        h = Input.GetAxis("Horizontal");
-        v = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime;
+        transform.Rotate(0, horizontal, 0);
 
-        Vector3 move = (Vector3.forward * v) + (Vector3.right * h);
-
-        tr.Translate(move * Time.deltaTime * Movespeed, Space.Self);
-        tr.Rotate(Vector3.up * Time.deltaTime * Rotspeed * Input.GetAxis("Mouse X"));
+        float vertical = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        transform.Translate(0, 0, vertical); ;
     }
+
+    //private float h = 0.0f;
+    //private float v = 0.0f;
+
+    //private Transform tr;
+
+    //float Movespeed = 5.0f;
+    //float Rotspeed = 150.0f;
+
+    //void Start()
+    //{
+    //    tr = GetComponent<Transform>();
+    //}
+
+    //void Update()
+    //{
+    //    h = Input.GetAxis("Horizontal");
+    //    v = Input.GetAxis("Vertical");
+
+    //    Vector3 move = (Vector3.forward * v) + (Vector3.right * h);
+
+    //    tr.Translate(move * Time.deltaTime * Movespeed, Space.Self);
+    //    tr.Rotate(Vector3.up * Time.deltaTime * Rotspeed * Input.GetAxis("Mouse X"));
+    //}
 }
