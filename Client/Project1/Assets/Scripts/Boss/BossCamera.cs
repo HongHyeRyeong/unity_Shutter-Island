a440f00c9f297585e3c27d00473967593ffd5940
HongHyeRyeong <h0412l@naver.com>
2018-01-28 17:19:54 +0900

    Boss Camera

---
+++ Client/Project1/Assets/Scripts/Boss/BossCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCamera : MonoBehaviour {
+    
+	void Start () {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	void Update () {
+		
+	}
+}
