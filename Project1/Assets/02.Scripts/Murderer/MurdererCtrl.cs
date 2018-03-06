03c88c72957b0ed5bb278e0ec149427070232df3
HongHyeRyeong <h0412l@naver.com>
2018-03-06 22:44:00 +0900

    캐릭터 애니메이션 완료

---
+++ Project1/Assets/02.Scripts/Murderer/MurdererCtrl.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MurdererCtrl : MonoBehaviour
+{
+    private Animator ani;
+
+    private int State;
+    private float Hp = 200f;
+    float MoveSpeed = 4.5f;
+    bool isAttack = false;
+
+    // 카메라
+    private float x;
+    float xSpeed = 100.0f;
+
+    //
+    const int State_Idle = 0;
+    const int State_Run = 1;
+    const int State_Parry = 2;
+    const int State_AttackW = 10;
+    const int State_AttackL = 11;
+
+    void Start()
+    {
+        ani = this.gameObject.GetComponent<Animator>();
+
+        State = State_Idle;
+    }
+
+    void Update()
+    {
+        // Movement
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        x += Input.GetAxis("Mouse X") * xSpeed * 0.015f;
+
+        if (State == State_Run)
+            transform.Translate(new Vector3(h, 0, v) * MoveSpeed * Time.deltaTime);
+
+
+        if (State != State_Parry)
+        {
+            Quaternion rotation = Quaternion.Euler(0, x, 0);
+            transform.rotation = rotation;
+        }
+
+        // State
+        if ((ani.GetCurrentAnimatorStateInfo(0).IsName("AttackW") ||
+                ani.GetCurrentAnimatorStateInfo(0).IsName("AttackL") ||
+                ani.GetCurrentAnimatorStateInfo(0).IsName("Parry")) &&
+                ani.GetCurrentAnimatorStateInfo(0).normalizedTime >= 0.8f)
+        {
+            ani.SetBool("isAttackW", false);
+            ani.SetBool("isAttackL", false);
+            ani.SetBool("isParry", false);
+            isAttack = false;
+
+            State = State_Idle;
+        }
+
+        if (State == State_Idle || State == State_Run)
+        {
+            if (v != 0 || h != 0)
+            {
+                State = State_Run;
+                ani.SetBool("isRun", true);
+            }
+            else
+            {
+                State = State_Idle;
+                ani.SetBool("isRun", false);
+            }
+        }
+
+        if (State != State_AttackW && State != State_AttackL && State != State_Parry)
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                State = State_AttackW;
+                ani.SetBool("isAttackW", true);
+                isAttack = true;
+            }
+            else if (Input.GetMouseButtonDown(1))
+            {
+                State = State_AttackL;
+                ani.SetBool("isAttackL", true);
+                isAttack = true;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.tag == "Survivor" && isAttack)
+        {
+            isAttack = false;
+            col.gameObject.GetComponent<SurvivorCtrl>().AttackByBoss(State);
+        }
+    }
+
+    public void DamageByPlayer(float Power)
+    {
+        State = State_Parry;
+        ani.SetBool("isParry", true);
+        Hp -= Power;
+    }
+}
---
+++ Project1/Assets/02.Scripts/Survivor/SurvivorCtrl.cs
@@ -0,0 +1,260 @@
+﻿using UnityEngine;
+using System.Collections;
+using NetworkModule;
+using GameServer;
+
+public class SurvivorCtrl : MonoBehaviour
+{
+    NetworkManager network_manager;
+
+    private Animator ani;
+
+    private int Character;
+    private int State;
+
+    private int Life = 2;
+    private float Hp = 100f;
+    private float Power = 10;
+    private float Stamina = 4f;
+    private float maxStamina;
+    private float MoveSpeed = 4f;
+    private int WorkSpeed = 10;
+
+    float AttackTime = 0.5f;
+
+    private bool Prison = false;
+    private bool PrisonTP = false;
+
+    //
+    const int Cha_Default = 0;
+    const int Cha_Stamina = 1;
+    const int Cha_WorkSpeed = 2;
+    const int Cha_Damage = 3;
+
+    const int State_Idle = 0;
+    const int State_SlowRun = 1;
+    const int State_Run = 2;
+    const int State_Hit = 3;
+    const int State_AttackW = 10;
+    const int State_AttackL = 11;
+
+    void Start()
+    {
+        ani = this.gameObject.GetComponent<Animator>();
+
+        Character = Cha_Default;
+        State = State_Idle;
+
+        if (Character == Cha_Stamina)
+        {
+            Stamina = 6f;
+        }
+        else if (Character == Cha_WorkSpeed)
+        {
+            WorkSpeed = 15;
+        }
+        else if (Character == Cha_Damage)
+        {
+            Power = 15;
+        }
+
+        maxStamina = Stamina;
+    }
+
+    private void Awake()
+    {
+        //this.network_manager = GameObject.Find("NetworkManager").GetComponent<NetworkManager>();
+    }
+
+    void Update()
+    {
+        // Movement
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        float rotationSpeed = 100f;
+
+        if (State != State_AttackW && State != State_AttackL)
+            transform.Rotate(0, h * rotationSpeed * Time.deltaTime, 0);
+
+        if (State == State_Run|| State == State_SlowRun)
+            transform.Translate(0, 0, v * MoveSpeed * Time.deltaTime);
+
+        // State
+
+        if (ani.GetCurrentAnimatorStateInfo(0).IsName("AttackW") ||
+                ani.GetCurrentAnimatorStateInfo(0).IsName("AttackL") ||
+                ani.GetCurrentAnimatorStateInfo(0).IsName("Hit"))
+        {
+            ani.SetBool("isAttackW", false);
+            ani.SetBool("isAttackL", false);
+            ani.SetBool("isHit", false);
+
+            State = State_Idle;
+        }
+
+        if (State == State_AttackW || State == State_AttackL)
+        {
+            AttackTime -= Time.deltaTime;
+
+            if (AttackTime > 0)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    if (State == State_AttackL)
+                        DamageByBoss();
+                    else
+                    {
+                        ani.SetBool("isAttackW", true);
+                        GameObject.Find("Murderer").GetComponent<MurdererCtrl>().DamageByPlayer(Power);
+                    }
+                }
+                else if (Input.GetMouseButtonDown(1))
+                {
+                    if (State == State_AttackW)
+                        DamageByBoss();
+                    else
+                    {
+                        ani.SetBool("isAttackL", true);
+                        GameObject.Find("Murderer").GetComponent<MurdererCtrl>().DamageByPlayer(Power);
+                    }
+
+                }
+            }
+            else
+            {
+                DamageByBoss();
+            }
+        }
+        else
+        {
+            if (v != 0 || h != 0)
+            {
+                State = State_SlowRun;
+                ani.SetBool("isSlowRun", true);
+            }
+            else
+            {
+                State = State_Idle;
+                ani.SetBool("isSlowRun", false);
+                ani.SetBool("isRun", false);
+            }
+        }
+
+        if (Prison)
+            PrisonTrue();
+
+        InputGet();
+
+        // End
+        if (Life == 0 || Hp <= 0)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public void SetState(int s)
+    {
+        State = s;
+    }
+
+    public void InputGet()
+    {
+        // Stamina
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            MoveSpeed = 4f;
+
+            if (State == State_Run)
+            {
+                State = State_SlowRun;
+            }
+            ani.SetBool("isRun", false);
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            if (State == State_SlowRun || State == State_Run)
+            {
+                if (Stamina > 0)
+                {
+                    State = State_Run;
+                    ani.SetBool("isRun", true);
+
+                    MoveSpeed = 6f;
+                    Stamina -= Time.deltaTime;
+                }
+                else
+                {
+                    State = State_SlowRun;
+                    ani.SetBool("isRun", false);
+                    MoveSpeed = 4f;
+                }
+            }
+        }
+        else
+        {
+            if (Stamina < maxStamina)
+                Stamina += 0.1f * Time.deltaTime;
+        }
+    }
+
+    public void AttackByBoss(int BossState)
+    {
+        State = BossState;
+        AttackTime = 0.5f;
+    }
+
+    void DamageByBoss()
+    {
+        State = State_Idle;
+
+        if (Life != 0 && Prison == false)
+        {
+            if (Hp == 100f)
+            {
+                State = State_Hit;
+                ani.SetBool("isHit", true);
+                Hp = 50f;
+            }
+            else if (Hp == 50f)
+            {
+                Life -= 1;
+                Prison = true;
+            }
+        }
+    }
+
+    void PrisonTrue()
+    {
+        Hp -= 0.5f * Time.deltaTime;
+
+        if (PrisonTP == false)
+        {
+            PrisonTP = true;
+
+            GameObject[] respawns = GameObject.FindGameObjectsWithTag("Prison");
+            Transform minTransform = transform;
+            float minDist = 10000f;
+
+            foreach (GameObject respawn in respawns)
+            {
+                float dist = Vector3.Distance(transform.position, respawn.transform.position);
+
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    minTransform = respawn.transform;
+                }
+            }
+
+            transform.position = minTransform.position;
+        }
+    }
+
+    void PrisonFalse()
+    {
+        Prison = false;
+        Hp = 50;
+    }
+}
