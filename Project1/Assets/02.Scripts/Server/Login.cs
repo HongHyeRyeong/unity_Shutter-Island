1f5868f19b4e5f5ead0f9e2e3110a9159612342c
hajeongyeon <hjy---@hanmail.net>
2018-06-02 18:01:59 +0900

    로그인 추가

---
+++ Project1/Assets/02.Scripts/Server/Login.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour {
+
+    public InputField IDInputField;
+    public InputField PWInputField;
+
+    public InputField NewIDInputField;
+    public InputField NewPWInputField;
+
+    public string LoginURL;
+
+    void Start()
+    {
+        LoginURL = "wjddus0424.dothome.co.kr/Login.php";
+    }
+
+    public void LoginButton()
+    {
+        StartCoroutine(LoginGo());
+    }
+
+    IEnumerator LoginGo()
+    {
+        print(IDInputField.text);
+        print(PWInputField.text);
+
+        WWWForm form = new WWWForm();
+        form.AddField("Input_id", IDInputField.text);
+        form.AddField("Input_pw", PWInputField.text);
+
+        WWW webRequest = new WWW(LoginURL, form);
+        yield return webRequest;
+
+        print(webRequest.text);
+
+        yield return null;
+    }
+
+    public void CreateAccountButton()
+    {
+        //StartCoroutine(LoginGo());
+    }
+}
