1567603c589faa4a6cae22cac3225c15a11e3f74
hajeongyeon <hjy---@hanmail.net>
2018-06-27 00:47:12 +0900

    로비 UI (test scene)

--- Project1/Assets/02.Scripts/Server/CharacterSelect.cs
+++ Project1/Assets/02.Scripts/Server/CharacterSelect.cs
@@ -7,12 +7,24 @@ public class CharacterSelect : MonoBehaviour {
     public GameObject survivor;
     public GameObject murderer;
 
+    public GameObject Surbtn1;
+    public GameObject Surbtn2;
+    public GameObject Surbtn3;
+    public GameObject Murbtn1;
+    public GameObject Murbtn2;
+
     static public bool SelSur = false;
 
 	public void OnClickSurvivor()
     {
         survivor.gameObject.SetActive(true);
         murderer.gameObject.SetActive(false);
+
+        Surbtn1.gameObject.SetActive(true);
+        Surbtn2.gameObject.SetActive(true);
+        Surbtn3.gameObject.SetActive(true);
+        Murbtn1.gameObject.SetActive(false);
+        Murbtn2.gameObject.SetActive(false);
 
         SelSur = true;
     }
@@ -21,6 +33,12 @@ public class CharacterSelect : MonoBehaviour {
     {
         survivor.gameObject.SetActive(false);
         murderer.gameObject.SetActive(true);
+
+        Murbtn1.gameObject.SetActive(true);
+        Murbtn2.gameObject.SetActive(true);;
+        Surbtn1.gameObject.SetActive(false);
+        Surbtn2.gameObject.SetActive(false);
+        Surbtn3.gameObject.SetActive(false);
 
         SelSur = false;
     }
